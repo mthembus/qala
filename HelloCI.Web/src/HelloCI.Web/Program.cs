4affb5e8933bfac208c85ae39dc2aad80ef35a82
sibusiso.mthembu <sibusiso.mthembu@gmail.com>
2017-05-24 20:18:43 +0200

    my changes

--- HelloCI.Web/src/HelloCI.Web/Program.cs
+++ HelloCI.Web/src/HelloCI.Web/Program.cs
@@ -15,7 +15,7 @@ namespace HelloCI.Web
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseStartup<Startup>()
+                .UseStartup<Startup>
                 .Build();
 
             host.Run();
